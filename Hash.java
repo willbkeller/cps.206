ae6a355ac34752aede69d5f9aaceceb0e3d6103b
William Keller <williamkeller@Williams-MacBook-Pro.local>
2017-02-01 19:23:03 -0600

    hash not complete

---
+++ Hash.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Hash{
+
+   public static void main(String[] args){
+      
+      ArrayList<String>[]names = new String[17];
+      int friday = "#Friday".hashCode() % names.length;
+      names[friday] = new ArrayList<>();
+      names[friday].add(post);
+   }
+}
+
+//java.util.Hashmap;
