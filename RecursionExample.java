c0aeb03911e88ef7edc1c5812358ea841aa11f36
William Keller <williamkeller@Williams-MacBook-Pro.local>
2017-02-02 08:51:29 -0600

    second commit

--- RecursionExample.java
+++ RecursionExample.java
@@ -4,6 +4,7 @@ public class RecursionExample{
    
       RecursionExample recursion = new RecursionExample();
       System.out.println(recursion.doFactorial(5));
+      System.out.println(recursion.doSum(recursion.sumRange(5, 10)));
    }
    
    public int doFactorial (int n){
@@ -11,6 +12,15 @@ public class RecursionExample{
       if(n==1) return 1;
       return n*=doFactorial(n-1);
       
+   }
+   
+   public int doSum(int n){
+     
+      return (n*n + n)/(2);
+   }
+   
+   public int sumRange(int f, int t){
+   
    }
   
 }
