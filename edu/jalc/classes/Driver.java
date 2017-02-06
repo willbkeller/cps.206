126afb0e065e20b82c7cc41ca077bc09acdd0e41
William Keller <williamkeller@Williams-MacBook-Pro.local>
2017-02-06 08:42:44 -0600

    packaging

---
+++ edu/jalc/classes/Driver.java
@@ -0,0 +1,9 @@
+package edu.jalc.classes;
+public class Driver{//now others can access
+
+   public static void main(String[] args){
+   
+      UselessClass uselessClass = new UselessClass();
+   
+   }
+}
---
+++ edu/jalc/classes/UselessClass.java
@@ -0,0 +1,12 @@
+package edu.jalc.classes;
+class UselessClass{}//dont need public for [default]
+//public makes it accesible to everyone. It is a visibility modifier 4)
+
+/*private - class itself can only access it. most restrictive 1)
+
+  protected- package and subclasses can access it, class itself 3)
+             
+  package.private[default]- only the package can access it, class itself 2)
+  
+  Why do we have that?*/
+
